d686b643a9d048197d5962f7e4c4d594a248266a
fiorela-18 <1603638@senati.pe>
2025-05-29 11:24:53 -0500

    facturador

---
+++ resources/js/Pages/Cliente/Index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head,link } from '@inertiajs/vue3';
+
+const page = usePage()
+
+console.log(page.props.clientes);
+</script>
+
+
+<template>
+    <AuthenticatedLayout>
+        <div class="py-12">
+            <div class="mx-auto max-w-7x1 sm:px-6 lg:px-8">
+                <div>
+                    <ul v-for="cliente in page.props.clientes.data">
+                        <li>{{ cliente }}</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </AuthenticatedLayout>
+
+</template>
